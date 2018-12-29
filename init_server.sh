a2176e5a740fd398a5275a80f581c63d580cf951
Mateusz.Galasinski <mateusz.galasinski@protonmail.com>
2018-12-29 16:25:05 +0100

    shorter await, ; added

--- init_server.sh
+++ init_server.sh
@@ -2,12 +2,12 @@
 
 ./entrypoint.sh mysqld &
 
-sleep 60s
+sleep 40s
 
-mysql -h "localhost" --connect-expired-password -u "root" "-pMyNewPass" < "user.sql"
-mysql -h "localhost" --connect-expired-password -u "root" "-pMyNewPass" < "table_setup.sql"
-mysql -h "localhost" --connect-expired-password -u "root" "-pMyNewPass" < "priviliges.sql"
+mysql -h "localhost" --connect-expired-password -u "root" "-pMyNewPass" < "user.sql";
+mysql -h "localhost" --connect-expired-password -u "root" "-pMyNewPass" < "table_setup.sql";
+mysql -h "localhost" --connect-expired-password -u "root" "-pMyNewPass" < "priviliges.sql";
 
-echo "Users database is ready for use. It will be running for 10 days or until container stops running"
+echo "Users database is ready for use. It will be running for 10 days or until container stops running";
 
 sleep 10d
